274e811aa2120ca39fcfb57db78589215b70e327
Josue <josue.nassar@stonybrook.edu>
2022-05-25 19:15:17 -0400

    utils file with code for computing simple metrics

---
+++ svmc/utils.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def compute_ess(weights):
+    return 1 / torch.sum(weights ** 2, -1)
+
+
+def compute_mse(X_true, X_particles, w_norm):
+    X_est = torch.sum(X_particles * w_norm.unsqueeze(2), 1)
+    return torch.mean((X_true - X_est) ** 2, -1)
+
+
+def compute_elbo(weights):
+    w_mean = torch.mean(weights, -1)
+    elbo = torch.log(torch.cumprod(w_mean, 0))
+    return elbo
